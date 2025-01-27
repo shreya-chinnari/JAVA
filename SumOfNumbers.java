56ec454d31f946cc839f7b692bff9df3eab4b692
Shreya <shreya16180@gmail.com>
2025-01-27 19:44:06 +0530

    Initial commit

---
+++ SumOfNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 5;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
