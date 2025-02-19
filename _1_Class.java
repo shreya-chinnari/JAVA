90fc7074bf467629d40bbc6a431450ba6610fe7a
Shreya <shreya16180@gmail.com>
2025-02-20 01:16:09 +0530

    _2_MainFunction

---
+++ _1_Class.java
@@ -0,0 +1,16 @@
+public class _1_Class {
+   
+}
+
+// class --> starts with capital letter
+// A class name must start with a letter (A-Z or a-z), an underscore _, or a dollar sign $.
+// It cannot start with a digit (0-9).
+// It can contain letters, digits, underscores, and dollar signs.
+
+// If a class is public, its filename must match the class name (including case) and have a .java extension.
+// FileName === public lass FileName {...}
+
+// A class name cannot be a Java reserved keyword (e.g., class, public, static, etc.).
+
+// public --> can be accessed from other files, packages, classes
+
---
+++ _2_MainFunction.java
@@ -0,0 +1,22 @@
+public class _2_MainFunction {
+   public static void main(String[] args) {
+      System.out.println("Hey");
+   }
+}
+
+
+// If no error, the code runs successfully, generating a FileName.class.
+// Class → A blueprint containing properties and methods/functions.
+// _2_MainFunction → The class name, matching the filename.
+// Method → A function inside a class (e.g., main).  ---------- {public static .... {}} 
+// main Method → The entry point of a Java program; that is run first
+// public → Allows main to be accessible from anywhere |  it holds main function, so it should be able to be used by anyone anywhere
+// static → Runs without creating an object of _2_MainFunction.
+// Main : first entry point | we need 'object' of the class to define variables, functions on it | static : static variables and functiosn do NOT depend on the OBJECT | so when we need to run {public static ....{}} first then we need tor un it without an object of _2_MainFunction class
+// void → return type of the function, here we dont need any return value hence void
+// (String[]) --> array of strings | whatever commands we give in terminal it is stored as an array in (String[])
+
+// images 1,2
+
+
+// source code (.java) goes in src folder, and byte code (.class) goes in output folder | terminal command
