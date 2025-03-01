36006ac5d53b63fd46db015e6073cdbf7e8a3338
Shreya <shreya16180@gmail.com>
2025-03-01 17:08:17 +0530

    conditionals

--- _5_If_Else.java
+++ _5_If_Else.java
@@ -1,3 +1,25 @@
+public class _5_If_Else{
+
+
 public static void main(String[] args) {
-   
+   /*
+    
+    Syntax : 
+      if(boolean expression T/F) {
+         // body
+      } else { 
+      
+      }
+
+    */
+   int salary = 10000;
+
+    if (salary < 10000) {
+      salary += 2000;
+    } else {
+      salary += 1000;
+    }
+
+    System.out.println(salary);
+}
 }
